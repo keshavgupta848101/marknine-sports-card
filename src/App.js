9f0330c8b3cb5e55ff81bed5773d182d1b475bc5
KESHAV GUPTA <iamkeshavgupta27@gmail.com>
2022-09-03 12:57:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const fakeDb = {
+  Cricket: [
+    { name: "Dhoni", country: "India" },
+    { name: "Rohit Sharma", country: "India" },
+    { name: "Virat Kohli", country: "India" },
+    { name: "Suresh Raina", country: "India" },
+    { name: "Bhuvneshwar Kumar", country: "India" }
+  ],
+
+  Badminton: [
+    {
+      name: "Saina Nehwal",
+      country: "India"
+    },
+    {
+      name: "P.V Sindhu",
+      country: "India"
+    },
+    {
+      name: "Saina Nehwal",
+      country: "India"
+    },
+    {
+      name: "Jwala Gutta",
+      country: "India"
+    },
+    {
+      name: "Pullela Gopichand",
+      country: "India"
+    }
+  ],
+  TableTennis: [
+    {
+      name: "Manav Thakkar",
+      country: "India"
+    },
+    {
+      name: "Sathiyan Gnanasekaran",
+      country: "India"
+    },
+    {
+      name: "Mouma Das",
+      country: "India"
+    },
+    {
+      name: "Achanta Sharath Kamal.",
+      country: "India"
+    }
+  ],
+  Football: [
+    {
+      name: "Climax Lawrence",
+      country: "India"
+    },
+    {
+      name: "Shabbir Ali",
+      country: "India"
+    },
+    {
+      name: "Bhaichung Bhutia",
+      country: "India"
+    },
+    {
+      name: "Sunil Chhetri",
+      country: "India"
+    }
+  ]
+};
+
+export default function App() {
+  const [category, setCategory] = useState("Cricket");
+
+  function switchCategory(category) {
+    console.log(category);
+    setCategory(category);
+  }
+
+  console.log();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sports Category 🏅</h1>
+      <p>Checkout my favorite players. Select a sports to get started</p>
+
+      {Object.keys(fakeDb).map((data) => {
+        return <button onClick={() => switchCategory(data)}>{data}</button>;
+      })}
+
+      <div>
+        <ul>
+          {fakeDb[category].map((players) => (
+            <li className="liCard">
+              <div>{players.name}</div>
+              <hr />
+              <div>{players.country}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
-
-export default App;
